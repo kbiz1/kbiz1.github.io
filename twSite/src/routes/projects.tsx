bb1920f49c9272038f0c8a482c5251f0427eb196
kbiz1 <kbusiness5mail@proton.me>
2024-10-03 12:57:41 +0000

    Upload production site to main

--- twSite/src/routes/projects.tsx
+++
@@ -1,8 +0,0 @@
-export default function Projects() {
-    return (
-        <div id="projects">
-            <h1 className="text-5xl">Projects</h1>
-            <button className="mt-4 p-0.5 rounded-md bg-offwhite" onClick={() => window.location.href="/Counter"}>Counter</button>
-        </div>
-    );
-}
--- twSite/src/routes/root.tsx
+++
@@ -1,8 +0,0 @@
-export default function HomePage() {
-    return (
-        <div id="homepage">
-            <button className="text-5xl animate-rotateColors" onClick={() => window.location.href="/Projects"}>kbiz1</button>
-            <p className="text-2xl pt-5">Software Developer</p>
-        </div>
-    );
-}
